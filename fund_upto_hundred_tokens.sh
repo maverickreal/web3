8a869e69683e4bf82cd37e577554b6edb8801cb2
Aditya Saxena <adity09@outlook.com>
2023-04-09 14:46:42 +0530

    better shell script

--- fund_upto_hundred_tokens.sh
+++ fund_upto_hundred_tokens.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-for i in {1..100}
+echo "Enter the number (>=0) of tokens to fund to the server's wallet: "
+read num
+
+for ((i=1; i<=$num; i++))
 do
-	echo "Airdropping $i th token from testnet";
+	echo "Airdropping $i th token from testnet"
 	solana airdrop 1 6wcKkenXuCapR5wGrWstay2VqacLqwBq48DVBqnAyV5d --url testnet
 done
